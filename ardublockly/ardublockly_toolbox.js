46f885f5d8a81ced1431471843ac323b403a3dc6
Nadar <adrien.bracq@gmail.com>
2018-04-04 23:21:28 +0200

    Impovement of the toolbox menu
    Modification of the toolbox menu with level selction.

--- ardublockly/ardublockly_toolbox.js
+++ ardublockly/ardublockly_toolbox.js
@@ -46,14 +46,14 @@ Ardublockly.changeToolbox = function(){
 		'		</value>' +
 		'    </block>' +
 		// '    <block type="scott_right">' +
-		// '      <value name="angle">' +
+		// '      <value name="VALUE">' +
 		// '        <shadow type="math_number">' +
 		// '          <field name="NUM">90</field>' +
 		// '        </shadow>' +
 		// '      </value>' +
 		// '    </block>' +
 		// '    <block type="scott_left">' +
-		// '      <value name="angle">' +
+		// '      <value name="VALUE">' +
 		// '        <shadow type="math_number">' +
 		// '          <field name="NUM">90</field>' +
 		// '        </shadow>' +
@@ -70,16 +70,16 @@ Ardublockly.changeToolbox = function(){
 		// '    <block type="scott_descendre_crayon"></block>' +
 		'  </category>' +
 		'  <sep></sep>' +
-		'  <category id="catLogic" name="Logic">' +
-		'    <block type="controls_if"></block>' +
-		'    <block type="logic_compare"></block>' +
-		'    <block type="logic_operation"></block>' +
-		'    <block type="logic_negate"></block>' +
-		'    <block type="logic_boolean"></block>' +
-		'    <block type="logic_null"></block>' +
-		'    <block type="logic_ternary"></block>' +
-		'  </category>' +
-		'  <sep></sep>' +
+		// '  <category id="catLogic" name="Logic">' +
+		// '    <block type="controls_if"></block>' +
+		// '    <block type="logic_compare"></block>' +
+		// '    <block type="logic_operation"></block>' +
+		// '    <block type="logic_negate"></block>' +
+		// '    <block type="logic_boolean"></block>' +
+		// '    <block type="logic_null"></block>' +
+		// '    <block type="logic_ternary"></block>' +
+		// '  </category>' +
+		//'  <sep></sep>' +
 		'  <category id="catLoops" name="Loops">' +
 		'    <block type="controls_repeat_ext">' +
 		'      <value name="TIMES">' +
@@ -88,69 +88,69 @@ Ardublockly.changeToolbox = function(){
 		'        </block>' +
 		'      </value>' +
 		'    </block>' +
-		'    <block type="controls_whileUntil"></block>' +
-		'    <block type="controls_for">' +
-		'      <value name="FROM">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">1</field>' +
-		'        </block>' +
-		'      </value>' +
-		'      <value name="TO">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">10</field>' +
-		'        </block>' +
-		'      </value>' +
-		'      <value name="BY">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">1</field>' +
-		'        </block>' +
-		'      </value>' +
-		'    </block>' +
-		'    <block type="controls_flow_statements"></block>' +
+		// '    <block type="controls_whileUntil"></block>' +
+		// '    <block type="controls_for">' +
+		// '      <value name="FROM">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">1</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '      <value name="TO">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">10</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '      <value name="BY">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">1</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '    </block>' +
+		// '    <block type="controls_flow_statements"></block>' +
 		'  </category>' +
 		'  <sep></sep>' +
 		'  <category id="catMath" name="Math">' +
 		'    <block type="math_number"></block>' +
-		'    <block type="math_arithmetic"></block>' +
-		'    <block type="math_single"></block>' +
-		'    <block type="math_trig"></block>' +
-		'    <block type="math_constant"></block>' +
-		'    <block type="math_number_property"></block>' +
-		'    <block type="math_change">' +
-		'      <value name="DELTA">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">1</field>' +
-		'        </block>' +
-		'      </value>' +
-		'    </block>' +
-		'    <block type="math_round"></block>' +
-		'    <block type="math_modulo"></block>' +
-		'    <block type="math_constrain">' +
-		'      <value name="LOW">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">1</field>' +
-		'        </block>' +
-		'      </value>' +
-		'      <value name="HIGH">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">100</field>' +
-		'        </block>' +
-		'      </value>' +
-		'    </block>' +
-		'    <block type="math_random_int">' +
-		'      <value name="FROM">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">1</field>' +
-		'        </block>' +
-		'      </value>' +
-		'      <value name="TO">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">100</field>' +
-		'        </block>' +
-		'      </value>' +
-		'    </block>' +
-		'    <block type="math_random_float"></block>' +
-		'    <block type="base_map"></block>' +
+		// '    <block type="math_arithmetic"></block>' +
+		// '    <block type="math_single"></block>' +
+		// '    <block type="math_trig"></block>' +
+		// '    <block type="math_constant"></block>' +
+		// '    <block type="math_number_property"></block>' +
+		// '    <block type="math_change">' +
+		// '      <value name="DELTA">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">1</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '    </block>' +
+		// '    <block type="math_round"></block>' +
+		// '    <block type="math_modulo"></block>' +
+		// '    <block type="math_constrain">' +
+		// '      <value name="LOW">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">1</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '      <value name="HIGH">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">100</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '    </block>' +
+		// '    <block type="math_random_int">' +
+		// '      <value name="FROM">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">1</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '      <value name="TO">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">100</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '    </block>' +
+		// '    <block type="math_random_float"></block>' +
+		// '    <block type="base_map"></block>' +
 		'  </category>' +
 		'  <sep></sep>' +
 		//'  <category id="catText" name="Text">' +
@@ -167,20 +167,20 @@ Ardublockly.changeToolbox = function(){
 		//'    <!--block type="text_print"></block Part of the serial comms -->' +
 		//'  </category>' +
 		//'  <sep></sep>' +
-		'  <category id="catVariables" name="Variables">' +
-		'    <block type="variables_get"></block>' +
-		'    <block type="variables_set"></block>' +
-		'    <block type="variables_set">' +
-		'      <value name="VALUE">' +
-		'        <block type="variables_set_type"></block>' +
-		'      </value>' +
-		'    </block>' +
-		'    <block type="variables_set_type"></block>' +
-		'  </category>' +
-		'  <sep></sep>' +
-		'  <category id="catFunctions" name="Functions" custom="PROCEDURE"></category>' +
-		'  <sep></sep>' +
-		// '  <category id="catInputOutput" name="Input/Output">' +
+		// '  <category id="catVariables" name="Variables">' +
+		// '    <block type="variables_get"></block>' +
+		// '    <block type="variables_set"></block>' +
+		// '    <block type="variables_set">' +
+		// '      <value name="VALUE">' +
+		// '        <block type="variables_set_type"></block>' +
+		// '      </value>' +
+		// '    </block>' +
+		// '    <block type="variables_set_type"></block>' +
+		// '  </category>' +
+		// '  <sep></sep>' +
+		// '  <category id="catFunctions" name="Functions" custom="PROCEDURE"></category>' +
+		// '  <sep></sep>' +
+		// // '  <category id="catInputOutput" name="Input/Output">' +
 		// '    <block type="io_digitalwrite">' +
 		// '      <value name="STATE">' +
 		// '        <block type="io_highlow"></block>' +
@@ -211,22 +211,22 @@ Ardublockly.changeToolbox = function(){
 		// '  </category>' +
 		// '  <sep></sep>' +
 		'  <category id="catTime" name="Time">' +
-		'    <block type="time_delay">' +
-		'      <value name="DELAY_TIME_MILI">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">1000</field>' +
-		'        </block>' +
-		'      </value>' +
-		'    </block>' +
-		'    <block type="time_delaymicros">' +
-		'      <value name="DELAY_TIME_MICRO">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">100</field>' +
-		'        </block>' +
-		'      </value>' +
-		'    </block>' +
-		'    <block type="time_millis"></block>' +
-		'    <block type="time_micros"></block>' +
+		// '    <block type="time_delay">' +
+		// '      <value name="DELAY_TIME_MILI">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">1000</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '    </block>' +
+		// '    <block type="time_delaymicros">' +
+		// '      <value name="DELAY_TIME_MICRO">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">100</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '    </block>' +
+		// '    <block type="time_millis"></block>' +
+		// '    <block type="time_micros"></block>' +
 		'    <block type="infinite_loop"></block>' +
 		'  </category>' +
 		'  <sep></sep>' +
@@ -251,20 +251,20 @@ Ardublockly.changeToolbox = function(){
 		'        </shadow>' +
 		'      </value>' +
 		'    </block>' +
-		'    <block type="scott_deplacement">' +
-		'		<value name="VALUE">' +
-		'       	<shadow type="math_number">' +
-		'          		<field name="NUM">90</field>' +
-		'        	</shadow>' +
-		'		</value>' +
-		'    </block>' +
-		'    <block type="scott_rotation">' +
-		'		<value name="angle">' +
-		'       	<shadow type="math_number">' +
-		'          		<field name="NUM">90</field>' +
-		'        	</shadow>' +
-		'		</value>' +
-		'    </block>' +
+		// '    <block type="scott_deplacement">' +
+		// '		<value name="VALUE">' +
+		// '       	<shadow type="math_number">' +
+		// '          		<field name="NUM">90</field>' +
+		// '        	</shadow>' +
+		// '		</value>' +
+		// '    </block>' +
+		// '    <block type="scott_rotation">' +
+		// '		<value name="angle">' +
+		// '       	<shadow type="math_number">' +
+		// '          		<field name="NUM">90</field>' +
+		// '        	</shadow>' +
+		// '		</value>' +
+		// '    </block>' +
 		'    <block type="scott_right">' +
 		'      <value name="VALUE">' +
 		'        <shadow type="math_number">' +
@@ -279,26 +279,26 @@ Ardublockly.changeToolbox = function(){
 		'        </shadow>' +
 		'      </value>' +
 		'    </block>' +
-		'    <block type="scott_stop"></block>' +
-		'    <block type="scott_turn_go"></block>' +
+		// '    <block type="scott_stop"></block>' +
+		// '    <block type="scott_turn_go"></block>' +
 		'    <block type="scott_crayon"></block>' +
-		'    <block type="scott_polygone"></block>' +
-		'    <block type="scott_cercle"></block>' +
-		'    <block type="scott_ligne"></block>' +
-		'    <block type="scott_contact"></block>' +
-		'    <block type="scott_lever_crayon"></block>' +
-		'    <block type="scott_descendre_crayon"></block>' +
+		// '    <block type="scott_polygone"></block>' +
+		// '    <block type="scott_cercle"></block>' +
+		// '    <block type="scott_ligne"></block>' +
+		// '    <block type="scott_contact"></block>' +
+		// '    <block type="scott_lever_crayon"></block>' +
+		// '    <block type="scott_descendre_crayon"></block>' +
 		'  </category>' +		'  <sep></sep>' +
-		'  <category id="catLogic" name="Logic">' +
-		'    <block type="controls_if"></block>' +
-		'    <block type="logic_compare"></block>' +
-		'    <block type="logic_operation"></block>' +
-		'    <block type="logic_negate"></block>' +
-		'    <block type="logic_boolean"></block>' +
-		'    <block type="logic_null"></block>' +
-		'    <block type="logic_ternary"></block>' +
-		'  </category>' +
-		'  <sep></sep>' +
+		// '  <category id="catLogic" name="Logic">' +
+		// '    <block type="controls_if"></block>' +
+		// '    <block type="logic_compare"></block>' +
+		// '    <block type="logic_operation"></block>' +
+		// '    <block type="logic_negate"></block>' +
+		// '    <block type="logic_boolean"></block>' +
+		// '    <block type="logic_null"></block>' +
+		// '    <block type="logic_ternary"></block>' +
+		// '  </category>' +
+		// '  <sep></sep>' +
 		'  <category id="catLoops" name="Loops">' +
 		'    <block type="controls_repeat_ext">' +
 		'      <value name="TIMES">' +
@@ -307,7 +307,7 @@ Ardublockly.changeToolbox = function(){
 		'        </block>' +
 		'      </value>' +
 		'    </block>' +
-		'    <block type="controls_whileUntil"></block>' +
+		// '    <block type="controls_whileUntil"></block>' +
 		'    <block type="controls_for">' +
 		'      <value name="FROM">' +
 		'        <block type="math_number">' +
@@ -325,37 +325,37 @@ Ardublockly.changeToolbox = function(){
 		'        </block>' +
 		'      </value>' +
 		'    </block>' +
-		'    <block type="controls_flow_statements"></block>' +
+		// '    <block type="controls_flow_statements"></block>' +
 		'  </category>' +
 		'  <sep></sep>' +
 		'  <category id="catMath" name="Math">' +
 		'    <block type="math_number"></block>' +
-		'    <block type="math_arithmetic"></block>' +
-		'    <block type="math_single"></block>' +
-		'    <block type="math_trig"></block>' +
-		'    <block type="math_constant"></block>' +
-		'    <block type="math_number_property"></block>' +
-		'    <block type="math_change">' +
-		'      <value name="DELTA">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">1</field>' +
-		'        </block>' +
-		'      </value>' +
-		'    </block>' +
-		'    <block type="math_round"></block>' +
-		'    <block type="math_modulo"></block>' +
-		'    <block type="math_constrain">' +
-		'      <value name="LOW">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">1</field>' +
-		'        </block>' +
-		'      </value>' +
-		'      <value name="HIGH">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">100</field>' +
-		'        </block>' +
-		'      </value>' +
-		'    </block>' +
+		// '    <block type="math_arithmetic"></block>' +
+		// '    <block type="math_single"></block>' +
+		// '    <block type="math_trig"></block>' +
+		// '    <block type="math_constant"></block>' +
+		// '    <block type="math_number_property"></block>' +
+		// '    <block type="math_change">' +
+		// '      <value name="DELTA">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">1</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '    </block>' +
+		// '    <block type="math_round"></block>' +
+		// '    <block type="math_modulo"></block>' +
+		// '    <block type="math_constrain">' +
+		// '      <value name="LOW">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">1</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '      <value name="HIGH">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">100</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '    </block>' +
 		'    <block type="math_random_int">' +
 		'      <value name="FROM">' +
 		'        <block type="math_number">' +
@@ -368,8 +368,8 @@ Ardublockly.changeToolbox = function(){
 		'        </block>' +
 		'      </value>' +
 		'    </block>' +
-		'    <block type="math_random_float"></block>' +
-		'    <block type="base_map"></block>' +
+		// '    <block type="math_random_float"></block>' +
+		// '    <block type="base_map"></block>' +
 		'  </category>' +
 		'  <sep></sep>' +
 		//'  <category id="catText" name="Text">' +
@@ -389,12 +389,12 @@ Ardublockly.changeToolbox = function(){
 		'  <category id="catVariables" name="Variables">' +
 		'    <block type="variables_get"></block>' +
 		'    <block type="variables_set"></block>' +
-		'    <block type="variables_set">' +
-		'      <value name="VALUE">' +
-		'        <block type="variables_set_type"></block>' +
-		'      </value>' +
-		'    </block>' +
-		'    <block type="variables_set_type"></block>' +
+		// '    <block type="variables_set">' +
+		// '      <value name="VALUE">' +
+		// '        <block type="variables_set_type"></block>' +
+		// '      </value>' +
+		// '    </block>' +
+		// '    <block type="variables_set_type"></block>' +
 		'  </category>' +
 		'  <sep></sep>' +
 		'  <category id="catFunctions" name="Functions" custom="PROCEDURE"></category>' +
@@ -430,22 +430,22 @@ Ardublockly.changeToolbox = function(){
 		// '  </category>' +
 		// '  <sep></sep>' +
 		'  <category id="catTime" name="Time">' +
-		'    <block type="time_delay">' +
-		'      <value name="DELAY_TIME_MILI">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">1000</field>' +
-		'        </block>' +
-		'      </value>' +
-		'    </block>' +
-		'    <block type="time_delaymicros">' +
-		'      <value name="DELAY_TIME_MICRO">' +
-		'        <block type="math_number">' +
-		'          <field name="NUM">100</field>' +
-		'        </block>' +
-		'      </value>' +
-		'    </block>' +
-		'    <block type="time_millis"></block>' +
-		'    <block type="time_micros"></block>' +
+		// '    <block type="time_delay">' +
+		// '      <value name="DELAY_TIME_MILI">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">1000</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '    </block>' +
+		// '    <block type="time_delaymicros">' +
+		// '      <value name="DELAY_TIME_MICRO">' +
+		// '        <block type="math_number">' +
+		// '          <field name="NUM">100</field>' +
+		// '        </block>' +
+		// '      </value>' +
+		// '    </block>' +
+		// '    <block type="time_millis"></block>' +
+		// '    <block type="time_micros"></block>' +
 		'    <block type="infinite_loop"></block>' +
 		'</xml>';
 	}
